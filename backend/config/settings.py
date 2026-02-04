5adc577807ff3855980815944d969b053e428f8f
Danilo Ivanisevic <daki248@gmail.com>
2026-02-04 12:56:37 +0100

    HTML browsable API added

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -125,6 +125,7 @@ STATIC_URL = 'static/'
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
     ],
     "DEFAULT_PARSER_CLASSES": [
         "rest_framework.parsers.JSONParser",
